e4c3f7c68bec85435147211dc186cacf97ce7fd6
Michelle M <89042600+michellemal@users.noreply.github.com>
2022-02-20 23:17:13 -0800

    Add files via upload

---
+++ build/precache-manifest.3a2c01a7f6939ae1f44c5dc9de9c9b78.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30fb3ac9a69d7b5cea368c3088235c26",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7aca6a353b2318840c07",
+    "url": "/static/css/main.9b442205.chunk.css"
+  },
+  {
+    "revision": "d94af6eecf5ea3a27fb0",
+    "url": "/static/js/2.f30e5af6.chunk.js"
+  },
+  {
+    "revision": "a90bb751b102ed9d8f9443a23ededd32",
+    "url": "/static/js/2.f30e5af6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7aca6a353b2318840c07",
+    "url": "/static/js/main.1de252a9.chunk.js"
+  },
+  {
+    "revision": "8f5431b127441a3e0cd9",
+    "url": "/static/js/runtime-main.c1ec362b.js"
+  },
+  {
+    "revision": "fdf4908d4b55b63ec286fea2be6f6a28",
+    "url": "/static/media/Felix.fdf4908d.png"
+  },
+  {
+    "revision": "e8ee6a5742de2767ebce087d4df88d53",
+    "url": "/static/media/FoundersHeader.e8ee6a57.png"
+  },
+  {
+    "revision": "891bd3e42830cfd4bac269a22ced5912",
+    "url": "/static/media/Kriselda.891bd3e4.png"
+  },
+  {
+    "revision": "84f28cd395b380e316e0603da6c18224",
+    "url": "/static/media/Michelle.84f28cd3.jpg"
+  },
+  {
+    "revision": "40aed4f9a4cbdff0f846e4cb16318558",
+    "url": "/static/media/Oscypki.40aed4f9.jpg"
+  },
+  {
+    "revision": "19cbc08ffc2b34b532e43759a85fb965",
+    "url": "/static/media/Ricky.19cbc08f.png"
+  },
+  {
+    "revision": "eee03ab3c4d41521c018d2aff7002acf",
+    "url": "/static/media/Sunrise.eee03ab3.png"
+  },
+  {
+    "revision": "34c290a576248c63cba68f0cd1c7557d",
+    "url": "/static/media/WelcomeHeader.34c290a5.png"
+  },
+  {
+    "revision": "93a7e5d238fc50e5bd0f4e7c284a2576",
+    "url": "/static/media/barrelStack.93a7e5d2.jpg"
+  },
+  {
+    "revision": "fd84414967f4ed887384c8f621c2f427",
+    "url": "/static/media/goatCheese.fd844149.jpg"
+  },
+  {
+    "revision": "d665321db7262c5306794756e92729ed",
+    "url": "/static/media/grapes.d665321d.jpg"
+  },
+  {
+    "revision": "a2fe9144ffedf33082b895452eab89f3",
+    "url": "/static/media/handHeldGrapes.a2fe9144.png"
+  },
+  {
+    "revision": "9e56f7431aa602ec9cc0ff584294ea80",
+    "url": "/static/media/mainLogo.9e56f743.png"
+  },
+  {
+    "revision": "28c7a2b504deebc003771ef663e2d2d0",
+    "url": "/static/media/navName.28c7a2b5.png"
+  },
+  {
+    "revision": "f912a0bba5f5e960ef6e72697b3185d6",
+    "url": "/static/media/vineyard.f912a0bb.jpg"
+  },
+  {
+    "revision": "9752c557689b45926bb73789f40e9c94",
+    "url": "/static/media/vineyardOverview.9752c557.png"
+  },
+  {
+    "revision": "c1bcc68c3f35bcccc557430df58200eb",
+    "url": "/static/media/wineBottle.c1bcc68c.jpg"
+  }
+]);
